aeeabef3bb0838f15ac9fde497aebf60874c45fe
40375408 <smoitie01@qub.ac.uk>
2023-07-07 17:10:50 +0100

    Latest code

---
+++ 3.test_all_button_combinations_with_text.py
@@ -0,0 +1,61 @@
+# Import Libraries
+from gpiozero import Button
+
+# Set up variabels for GPIO pins
+buttonA = Button(27)
+buttonB = Button(18)
+buttonC = Button(17)
+buttonD = Button(15)
+buttonE = Button(14)
+
+# Declare variables
+beamBroken = False
+running = True
+buttonList = []
+
+# Dictionary to hold the buttons and map each one with a name
+buttons = {
+    "Beam A": buttonA, 
+    "Beam B": buttonB, 
+    "Beam C": buttonC, 
+    "Beam D": buttonD, 
+    "Beam E": buttonE
+    }
+  
+# Function to output messages when beams are broken
+def beamMessages():
+    
+    global beamBroken
+    
+     # For loop to add each broken beam to new list
+    for button, beam in buttons.items():
+        if beam.is_pressed:
+            beamBroken = True
+            #print(button +" is pressed")
+            buttonList.append(button)
+                    
+    # Output message if no beams broken
+    if beamBroken == False:
+        print("All beams are connected")
+    # Output message if one or more beams are broken
+    elif beamBroken == True:
+        if len(buttonList) == 1:
+            print(buttonList[0] +" is broken")
+        elif len(buttonList) == 2:
+            print(buttonList[0] + " and " +buttonList[1] +" are broken")
+        elif len(buttonList) == 3:
+            print(buttonList[0] + ", " +buttonList[1], " and " +buttonList[2] +" are broken")
+        elif len(buttonList) == 4:
+            print(buttonList[0] +", " +buttonList[1] +", " +buttonList[2] + " and " +buttonList[3] + " are broken")
+        elif len(buttonList) == 5:
+            print(buttonList[0] +", " +buttonList[1] +", " +buttonList[2] +"," +buttonList[3] + " and " +buttonList[4] + " are broken")
+            
+    # Reset for next loop through        
+    beamBroken = False
+    buttonList.clear()
+        
+# While loop to keep program running
+while running:
+    
+    # Recall function to output message if beams broken
+    beamMessages()
